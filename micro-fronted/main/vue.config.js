90ac19cd192c9a91b4f5f5892d3c301e6752f323
maqiling <maqiling@shanda.com>
2021-08-04 23:28:23 +0800

    add docker

--- micro-fronted/main/vue.config.js
+++ micro-fronted/main/vue.config.js
@@ -1,5 +1,15 @@
+const { name } = require('../package.json')
+
 module.exports = {
   transpileDependencies: ['common'],
+  configureWebpack: {
+    output: {
+      // 把子应用打包成 umd 库格式
+      library: `${name}-[name]`,
+      libraryTarget: 'umd',
+      jsonpFunction: `webpackJsonp_${name}`
+    }
+  },
   chainWebpack: config => {
     config.plugin('html')
       .tap((args) => {
